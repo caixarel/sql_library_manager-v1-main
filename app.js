c6abb723abe47a42f40dd2faa2605bc15e46f2b7
caixarel <44868812+caixarel@users.noreply.github.com>
2021-08-09 17:06:24 +0200

    2 day routes set

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const sequelize = require('./models/index').sequelize;
 sequelize.sync();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,20 +25,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// app.get("/",(req,res)=>{
+//   res.render('layout');
+// })
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  //next(createError(404));
+  const error = new Error ('Page not found');
+  error.status=404;
+  next(error);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (err.status== 404){
+    err.message = 'Error occured , page not found.';
+    console.log('Error 404 page-not-found');
+    res.render('page_not_found',{err});
+}
+else{
+    err.message = 'Error , return to main page ';
+    err.status=500;
+    res.render('error',{err});
+}
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,43 @@ var router = express.Router();
 const Book = require('../models').Book;
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  const books = await Book.findAll();
-  console.log(books);
-  res.json(books); 
+  // //res.render('index', { title: 'Express' });
+  // const books = await Book.findAll();
+  // console.log(books);
+  // res.json(books); 
+    res.render('index');
+
 });
+router.get('/books', async function(req, res, next) {
+
+    res.render('form-error');
+
+});
+router.get('/books/new', async function(req, res, next) {
+
+  res.render('new-book');
+
+});
+router.post('/books/new', async function(req, res, next) {
+
+  res.render('layout');
+
+});
+router.get('/books/:id', async function(req, res, next) {
+
+  res.render('update-book');
+
+});
+router.post('/books/:id', async function(req, res, next) {
+
+  res.render('layout');
+
+});
+router.post('/books/:id/delete', async function(req, res, next) {
+
+  res.render('layout');
+
+});
+
 
 module.exports = router;
