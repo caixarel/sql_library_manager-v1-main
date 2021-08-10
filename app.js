6a63383a23717ed2d45792fc63bc566ecb3dc2a2
caixarel <44868812+caixarel@users.noreply.github.com>
2021-08-10 16:53:49 +0200

    search complete

--- app.js
+++ app.js
@@ -25,11 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// app.get("/",(req,res)=>{
-//   res.render('layout');
-// })
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,63 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require("sequelize");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-
     res.redirect('/books');
-
 });
+//Show the home page containing all the books
 router.get('/books', async function(req, res, next) {
+  //Retrieve array with all books from the database, then on the template
+  //iterate over the array to create a link to every book
   const books= await Book.findAll();
   res.render('index',{books});
-
 });
+
+router.post('/search', async function(req, res, next) {
+
+  const books = await Book.findAll({
+    attributes:['id','title','author','genre','year'],
+    where:{
+      [Op.or]:[{
+        title:{
+          [Op.like]:`%${req.body.search}%`}},
+        {
+        author:{
+          [Op.like]:`%${req.body.search}%`}},
+        {
+        genre:{
+          [Op.like]:`%${req.body.search}%`}},
+        {
+        year:{
+          [Op.like]:`%${req.body.search}%`}}
+      ]
+      }
+      
+  })
+  console.log(books.dataValues)
+  res.render('index',{books});
+});
+
+
+
+//Shows the page for the creation of new books
 router.get('/books/new', async function(req, res, next) {
-
   res.render('new-book');
-
 });
+//create a new book
 router.post('/books/new', async function(req, res, next) {
   try{
+    //creates a new book using the data from a form
     await Book.create({
       title: req.body.title,
       author: req.body.author,
       genre:req.body.genre,
       year:req.body.year
     })
+    res.redirect('/');
   }catch(error){
+    //if the user dont fill the mandatory form fields a message will be shown
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map(err => err.message);
       res.render('form-error',{errors});
@@ -33,23 +65,39 @@ router.post('/books/new', async function(req, res, next) {
       throw error;
     }
   }
- 
-  
 });
+
+//Will display a page containing the information of the selected book
 router.get('/books/:id', async function(req, res, next) {
-
-   res.render('update-book');
-
- });
- router.post('/books/:id', async function(req, res, next) {
-
-   res.render('layout');
+  const book = await Book.findByPk(req.params.id);
+   res.render('update-book',{book});
 
  });
+//Updates the book information using the user's input
+router.post('/books/:id', async function(req, res, next) {
+  const book = await Book.findByPk(req.params.id);
+  try{
+    await book.update(req.body);
+    res.redirect('/');
+  //When the user tries to update a book he must fill the mandatory fields otherwise
+  //a error message will be shown
+  }catch(error){
+    if (error.name === 'SequelizeValidationError') {
+      const errors = error.errors.map(err => err.message);
+      const oldBook= await Book.findByPk(req.params.id);
+      const bookValues =oldBook.dataValues;
+      res.render('form-error',{errors,bookValues});
+    } else {
+      throw error;
+    }
+  }
+ });
+
+ //Deletes a book from the database
 router.post('/books/:id/delete', async function(req, res, next) {
-
-  res.render('layout');
-
+  const book = await Book.findByPk(req.params.id);
+  await book.destroy();
+  res.redirect('/');
 });
 
 
